752b839b9cfe60a05eda818f472905d77a898dc1
xeiis <seis02@hotmail.fr>
2017-05-24 16:11:05 +0200

    Adding start nm fonction

--- src/nm.c
+++ src/nm.c
@@ -6,13 +6,41 @@
 /*   By: dchristo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/23 18:41:43 by dchristo          #+#    #+#             */
-/*   Updated: 2017/05/23 18:44:05 by dchristo         ###   ########.fr       */
+/*   Updated: 2017/05/24 16:08:28 by dchristo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/nm-otool.h"
 
-void	nm(void)
+void	nm(char *str)
 {
-	printf("nm\n");
+	(void)str;
+	ft_putstr("in nm\n");	
 }
+
+int		start_nm(char *file)
+{
+	int			fd;
+	struct stat buf;
+	char 		*ptr;
+
+	if ((fd = open(file, O_RDONLY)) < 0 ) 
+	{
+		perror("open");
+		return (EXIT_FAILURE);
+	}
+	if (fstat(fd, &buf) < 0)
+	{
+		perror("fstat");
+		return (EXIT_FAILURE);
+	}
+	if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) 
+		== MAP_FAILED)
+	{
+		perror("mmap");
+		return(EXIT_FAILURE);
+	}
+	nm(ptr);
+	return (EXIT_SUCCESS);
+}
+
