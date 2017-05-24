885d77bdef70a736fbf7a18a5c48a26af61a28da
xeiis <seis02@hotmail.fr>
2017-05-24 16:25:50 +0200

    Made norm

--- src/nm.c
+++ src/nm.c
@@ -6,25 +6,25 @@
 /*   By: dchristo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/23 18:41:43 by dchristo          #+#    #+#             */
-/*   Updated: 2017/05/24 16:08:28 by dchristo         ###   ########.fr       */
+/*   Updated: 2017/05/24 16:22:14 by dchristo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/nm-otool.h"
+#include "../include/nm_otool.h"
 
 void	nm(char *str)
 {
 	(void)str;
-	ft_putstr("in nm\n");	
+	ft_putstr("in nm\n");
 }
 
 int		start_nm(char *file)
 {
 	int			fd;
 	struct stat buf;
-	char 		*ptr;
+	char		*ptr;
 
-	if ((fd = open(file, O_RDONLY)) < 0 ) 
+	if ((fd = open(file, O_RDONLY)) < 0)
 	{
 		perror("open");
 		return (EXIT_FAILURE);
@@ -34,13 +34,14 @@ int		start_nm(char *file)
 		perror("fstat");
 		return (EXIT_FAILURE);
 	}
-	if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) 
+	if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0))
 		== MAP_FAILED)
 	{
 		perror("mmap");
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 	nm(ptr);
+	if (munmap(ptr, buf.st_size) < 0)
+		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
-
