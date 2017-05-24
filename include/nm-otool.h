3c974144670e4792df30bbdf4ec9b16b1fe6bb0a
xeiis <seis02@hotmail.fr>
2017-05-24 16:10:24 +0200

    Adding start_nm fonction and unistd library

--- include/nm-otool.h
+++ include/nm-otool.h
@@ -6,7 +6,7 @@
 /*   By: dchristo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/23 18:40:38 by dchristo          #+#    #+#             */
-/*   Updated: 2017/05/23 18:43:34 by dchristo         ###   ########.fr       */
+/*   Updated: 2017/05/24 16:07:27 by dchristo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,18 @@
 # define NM_OTOOL_H
 
 #include <stdio.h>
+#include <sys/mman.h>
+#include <mach-o/loader.h>
+#include <mach-o/nlist.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-void	nm(void);
+void	nm(char *str);
 void	otool(void);
+void	ft_putstr(char *str);
+void	ft_putnbr(long n);
+int		start_nm(char *file);
 
 #endif
