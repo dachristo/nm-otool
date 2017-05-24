aa02aad9184baad3c80f450c9dea5d20443fccdd
xeiis <seis02@hotmail.fr>
2017-05-24 16:25:09 +0200

    Modified name for norm

---
+++ include/nm_otool.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   nm_otool.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dchristo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/23 18:40:38 by dchristo          #+#    #+#             */
+/*   Updated: 2017/05/24 16:24:46 by dchristo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef NM_OTOOL_H
+# define NM_OTOOL_H
+
+# include <stdio.h>
+# include <sys/mman.h>
+# include <mach-o/loader.h>
+# include <mach-o/nlist.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+void	nm(char *str);
+void	otool(void);
+void	ft_putstr(char *str);
+void	ft_putnbr(long n);
+int		start_nm(char *file);
+
+#endif
