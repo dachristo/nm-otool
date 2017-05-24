7b6db24d5b7b17fcbb48cbfe3bb88988d052d8db
xeiis <seis02@hotmail.fr>
2017-05-24 17:57:02 +0200

    Finished video explication

--- src/nm.c
+++ src/nm.c
@@ -6,16 +6,61 @@
 /*   By: dchristo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/23 18:41:43 by dchristo          #+#    #+#             */
-/*   Updated: 2017/05/24 16:22:14 by dchristo         ###   ########.fr       */
+/*   Updated: 2017/05/24 17:56:43 by dchristo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/nm_otool.h"
 
-void	nm(char *str)
+void	print_out(int nsyms, int symoff, int stroff, char *ptr)
 {
-	(void)str;
-	ft_putstr("in nm\n");
+	int					i;
+	char				*stringtable;
+	struct nlist_64		*array;
+
+	array = (void *) ptr + symoff;
+	stringtable = (void *) ptr + stroff;
+	i = 0;
+	while (i < nsyms)
+	{
+		ft_putstr(stringtable + array[i].n_un.n_strx);
+		ft_putstr("\n");
+		i++;
+	}
+} 
+
+void	handle_64(char *ptr)
+{
+	int						ncmds;
+	int 					i;
+	struct mach_header_64	*header;
+	struct load_command		*lc;
+	struct symtab_command	*sym;
+
+	header = (struct mach_header_64 *) ptr;
+	ncmds = header->ncmds;
+	i = 0;
+	lc = (void *) ptr + sizeof(*header);
+	while (i < ncmds)
+	{
+		if (lc->cmd == LC_SYMTAB)
+		{
+			sym = (struct symtab_command *) lc;
+			print_out(sym->nsyms, sym->symoff, sym->stroff, ptr);
+			break ;
+		}
+		i++;
+		lc = (void *) lc + lc->cmdsize;
+	}
+}
+
+void	nm(char *ptr)
+{
+	unsigned int		magic_number;
+
+	magic_number = *(int *) ptr;
+	if (magic_number == MH_MAGIC_64)
+		handle_64(ptr);
 }
 
 int		start_nm(char *file)
