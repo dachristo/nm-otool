adc1e50130e28e43a61f177a3232aed388d330ef
xeiis <seis02@hotmail.fr>
2017-05-25 15:26:49 +0200

    Added ft_puthex and ft_strcmp

--- include/nm_otool.h
+++ include/nm_otool.h
@@ -6,7 +6,7 @@
 /*   By: dchristo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/23 18:40:38 by dchristo          #+#    #+#             */
-/*   Updated: 2017/05/24 16:27:45 by dchristo         ###   ########.fr       */
+/*   Updated: 2017/05/25 15:25:35 by dchristo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,13 @@
 # include <sys/stat.h>
 # include <stdlib.h>
 # include <unistd.h>
+# define FT_HEX "abcdef"
 
 void	nm(char *str);
 void	ft_putstr(char *str);
 void	ft_putnbr(long n);
 int		start_nm(char *file);
+int		ft_strcmp(const char *s1, const char *s2);
+void	ft_puthex(size_t n);
 
 #endif
