425d1705332e05b51a372c5fb925c3a04227a6fe
xeiis <seis02@hotmail.fr>
2017-05-30 19:23:09 +0200

    Added the sort for the list

--- src/nm.c
+++ src/nm.c
@@ -6,7 +6,7 @@
 /*   By: dchristo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/23 18:41:43 by dchristo          #+#    #+#             */
-/*   Updated: 2017/05/25 15:24:59 by dchristo         ###   ########.fr       */
+/*   Updated: 2017/05/30 19:22:04 by dchristo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,34 +20,75 @@ void	ft_putsymtype(char *str)
 		ft_putstr("T");
 	else if (ft_strcmp("", str) == 0)
 		ft_putstr("U");
+	else
+		ft_putstr(str);
 }
 
-void	print_out(int nsyms, int symoff, int stroff, char *ptr)
+
+void	print_out(int *tried, struct symtab_command *sym, char *ptr)
 {
 	int					i;
 	char				*stringtable;
 	struct nlist_64		*array;
 
-	array = (void *) ptr + symoff;
-	stringtable = (void *) ptr + stroff;
+	array = (void *) ptr + sym->symoff;
+	stringtable = (void *) ptr + sym->stroff;
 	i = 0;
-	while (i < nsyms)
+	while (tried[i] != -1)
 	{
-		if (array[i].n_value != 0)
+		if (array[tried[i]].n_value != 0)
 		{
 			ft_putstr("0000000");
-			ft_puthex(array[i].n_value); // value
+			ft_puthex(array[tried[i]].n_value); // value
 		}
 		else
 			ft_putstr("                ");
 		ft_putstr(" ");
-		ft_putsymtype(stringtable + array[i].n_type); // type
+		ft_putsymtype(stringtable + array[tried[i]].n_type); // type
 		ft_putstr(" ");
-		ft_putstr(stringtable + array[i].n_un.n_strx); // function_name
+		ft_putstr(stringtable + array[tried[i]].n_un.n_strx); // function_name
 		ft_putstr("\n");
 		i++;
 	}
-} 
+}
+
+int		*tri_before(int nsyms, int symoff, int stroff, char *ptr)
+{
+	int					i;
+	char				*stringtable;
+	struct nlist_64		*array;
+	int					*tried;
+	int					tmp;
+	int					j;
+	array = (void *) ptr + symoff;
+	stringtable = (void *) ptr + stroff;
+	i = 0;
+	tried = malloc(sizeof(int) * nsyms + 1);
+	while (i < nsyms)
+	{
+		tried[i] = i;
+		i++;
+	}
+	tried[i] = -1;
+	i = 0;
+	while (tried[i] != -1)
+	{
+		j = 0;
+		while (j < nsyms)
+		{
+			if (ft_strcmp(stringtable + array[tried[i]].n_un.n_strx, 
+				stringtable + array[tried[j]].n_un.n_strx) < 0)
+			{
+				tmp = tried[i];
+				tried[i] = tried[j];
+				tried[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (tried);
+}
 
 void	handle_64(char *ptr)
 {
@@ -66,7 +107,7 @@ void	handle_64(char *ptr)
 		if (lc->cmd == LC_SYMTAB)
 		{
 			sym = (struct symtab_command *) lc;
-			print_out(sym->nsyms, sym->symoff, sym->stroff, ptr);
+			print_out(tri_before(sym->nsyms, sym->symoff, sym->stroff, ptr), sym, ptr);
 			break ;
 		}
 		i++;
