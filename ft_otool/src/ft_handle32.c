d24343a69dbc4beba106fa67b372f4ee4b6d8321
dchristo <dchristo@student.42.fr>
2018-04-20 14:12:05 +0200

    correc handle32 same handle64

--- ft_otool/src/ft_handle32.c
+++ ft_otool/src/ft_handle32.c
@@ -12,11 +12,11 @@
 
 #include "../include/otool.h"
 
-static void			treatment(struct segment_command *segment,
-								char *ptr, char *file, int lib)
+static int		treatment(struct segment_command *segment,
+							   t_file_ptr *ptr_file, char *file, int lib)
 {
-	t_section			*section;
 	int					j;
+	struct section	*section;
 
 	section = (struct section*)((void*)segment + sizeof(*segment));
 	j = -1;
@@ -25,17 +25,20 @@ static void			treatment(struct segment_command *segment,
 		while (++j < (int)segment->nsects)
 		{
 			if (!ft_strcmp(section[j].sectname, SECT_TEXT))
-				print_hex(ptr, &section[j], file, lib);
+				print_hex(ptr_file->ptr, &section[j], file, lib);
 		}
 	}
 	segment = (void *)segment + segment->cmdsize;
+	if (check_ptr(segment, "extends past the end of the file\n", ptr_file) == 1)
+		return (1);
+	return (0);
 }
 
-static void			treatment_data(struct segment_command *segment,
-									char *ptr, char *file, int lib)
+static int		treatment_data(struct segment_command *segment,
+									t_file_ptr *ptr_file, char *file, int lib)
 {
-	t_section			*section;
 	int					j;
+	struct section	*section;
 
 	section = (struct section*)((void*)segment + sizeof(*segment));
 	j = -1;
@@ -44,36 +47,55 @@ static void			treatment_data(struct segment_command *segment,
 		while (++j < (int)segment->nsects)
 		{
 			if (!ft_strcmp(section[j].sectname, SECT_DATA))
-				print_hex_data(ptr, &section[j], file, lib);
+				print_hex_data(ptr_file->ptr, &section[j], file, lib);
 		}
 	}
 	segment = (void *)segment + segment->cmdsize;
+	if (check_ptr(segment, "extends past the end of the file\n", ptr_file) == 1)
+		return (1);
+	return (0);
 }
 
-int					ft_handle32(t_mach_header *header, t_file_ptr *ptr_file,
-								char *file, int lib)
+static int		handle_flag_d(struct load_command	*lc, t_file_ptr *ptr_file,
+								t_mach_header *header, char *file)
 {
-	int						i;
-	struct load_command		*lc;
+	int i;
+
+	i = 0;
+	lc = (void *)ptr_file->ptr + sizeof(struct mach_header);
+	while (i++ < (int)header->ncmds)
+	{
+		if (treatment_data((struct segment_command*)lc, ptr_file,
+							  file, -1) == 1)
+			return (1);
+		lc = (void *)lc + lc->cmdsize;
+		if (check_ptr(lc, "extends past the end of the file\n", ptr_file) == 1)
+			return (1);
+	}
+}
+
+int				ft_handle32(t_mach_header *header, t_file_ptr *ptr_file,
+							   char *file, int lib)
+{
+	int					i;
+	struct load_command	*lc;
 
 	i = -1;
 	lc = (void *)ptr_file->ptr + sizeof(*header);
 	while (++i < (int)header->ncmds)
 	{
-		treatment((struct segment_command*)lc, ptr_file->ptr, file, lib);
+		if (treatment((struct segment_command*)lc, ptr_file,
+						 file, lib) == 1)
+			return (1);
 		lc = (void *)lc + lc->cmdsize;
 		if (ptr_file->options->flag_d == TRUE)
 		{
-			i = 0;
-			lc = (void *)ptr_file->ptr + sizeof(struct mach_header);
-			while (i++ < (int)header->ncmds)
-			{
-				treatment_data((struct segment_command*)lc, ptr_file->ptr,
-							file, -1);
-				lc = (void *)lc + lc->cmdsize;
-			}
+			if (handle_flag_d(lc, ptr_file, header, file) == 1)
+				return (1);
 		}
-		if (check_ptr(lc, "extends past the end of the file\n", ptr_file) == 1)
+		if (check_ptr(lc, "extends past the end of the file\n",
+					  ptr_file) == 1 ||
+			(check_lc(lc->cmdsize, i, (int)header->ncmds, ptr_file) == 1))
 			return (1);
 	}
 	return (0);
